784f550013c3f06f90377f7a7c363d235c6b63fa
vasilev <vasilyev0992@gmail.com>
2020-02-13 16:30:24 +0300

    Созданы классы Puzzle (где запускается программа) и Search (метод поиска в ширину (BFS Breadth-First Search))

--- src/puzzle/Puzzle.java
+++ src/puzzle/Puzzle.java
@@ -3,7 +3,14 @@ package puzzle;
 /*
 Для решения задачи применяется поиск в ширину (BFS Breadth-First Search)
 С помощью тестов выяснил, что один из самых труднозатратных стартовых массивов: [1, 6, 3, 4, 0, 7, 2, 5]
-На данный момент решение находится в среднем за 650 ms
+На данный момент решение для него находится в среднем за 650 ms
+
+UPD: Т.к. нам известен конечный массив, можно запустить двунаправленный поиск в ширину.
+Один объект начинает поиск с старта до финиша, а второй с финиша до старта.
+После каждого цикла проверяется есть ли совпадения построенных комбинаций,
+если да, то берется маршрут до этой комбинации с старта + маршут до этой же комбинации с финиша.
+Таким образом время поиска сокрашается в несколько раз.
+К примеру по тому же массиву [1, 6, 3, 4, 0, 7, 2, 5] время поиска состовялет ~100ms против 650ms ранее
  */
 
 
@@ -13,11 +20,13 @@ public class Puzzle {
 
         long startTime =System.currentTimeMillis();
         int[] finish = new int[] {1, 2, 3, 4, 0, 5, 6, 7};
+        //int[] start = new int[] {0, 1, 2, 3, 4, 5, 6, 7};
         //int[] start = new int[] {2, 1, 3, 4, 0, 5, 6, 7};
         int[] start = new int[] {1, 6, 3, 4, 0, 7, 2, 5}; // самый сложный старт
 
-        Search search = new Search(start, finish);
-        System.out.println(search.outResult(search.run()));
+        Search search1 = new Search(start, finish);
+        Search search2 = new Search(finish, start);
+        System.out.println(Search.result(search1, search2));
 
         long timeSpent =System.currentTimeMillis()- startTime;
         System.out.println("Время выполнения: "+ timeSpent +" миллисекунд");
--- src/puzzle/Search.java
+++ src/puzzle/Search.java
@@ -6,16 +6,19 @@ public class Search {
    private int [] start;
    private int [] finish;
    private int combNumb;
+   private int firstCombNumb;
+   private int secondCombNumb;
    private int level;
    private Queue<int[]> queueList; // очередь комбинаций
    private ArrayList<Integer> HistoryMoves; // история ходов
    private ArrayList<Integer> LvlList;     // история уровней, нужна для восстановления истории ходов
-   private HashSet<String>VisitComb;  // массив для отслеживания уже проверенных комбинаций
+   private HashMap<String, Integer>VisitComb;  // массив для отслеживания уже проверенных комбинаций
 
     public Search(int[] start, int[] finish) {
         this.start = start;
         this.finish = finish;
         this.combNumb = 1;
+        this.secondCombNumb = 0;
         this.level = 0;
         this.queueList = new LinkedList<>();
         this.queueList.add(start);
@@ -23,39 +26,84 @@ public class Search {
         this.HistoryMoves.add(0,0);
         this.LvlList = new ArrayList<>();
         this.LvlList.add(0,0);
-        this.VisitComb = new HashSet<>();
-        this.VisitComb.add(null);
+        this.VisitComb = new HashMap<>();
+        this.VisitComb.put(null, 0);
+
+        if(Arrays.equals(start, finish)) this.firstCombNumb = 0;
+        else this.firstCombNumb = -1;
     }
 
-    public int run() {
+    public static String result(Search searchFromStart, Search searchFromFinish) {
 
-        while (queueList.size() > 0) { // поиск пока очередь не пуста
-            int[] comb = queueList.poll();
-            int zero = this.zeroCell(comb);
-            for (int i = 0; i < comb.length; i++) {
+        String result = "";
 
-                if (this.neighbors(zero, i) &&
-                        comb[i] != HistoryMoves.get(level)) {
-                    int[] temp = comb.clone();
-                    temp[i] = 0;
-                    temp[zero] = comb[i];
+        if(searchFromStart.firstCombNumb >= 0) {
+            if(searchFromStart.secondCombNumb > 0) // если поиск удался с двух объектов
+                result = searchFromStart.resultList(searchFromStart.firstCombNumb,
+                                                    searchFromStart.secondCombNumb,
+                                                    searchFromFinish
+                                                    ).toString();
 
-                    if (VisitComb.add(Arrays.toString(temp))) { // true при добавлении, иначе false
-                        queueList.add(temp);
-                        HistoryMoves.add(combNumb, comb[i]);
-                        LvlList.add(combNumb, level);
-                        if (Arrays.equals(temp, finish)) return combNumb; // возвращает номер комбинации
-                        combNumb++;
-                    }
-                }
-            } level++;
+            else { // если поиск удался с одного объекта
+                ArrayList<Integer> listRes = searchFromStart.resultList(searchFromStart.firstCombNumb);
+                Collections.reverse(listRes);  // требуется перевернуть массив т.к. эта история передвижений с конца
+                result = listRes.toString();
+            }
+
         }
-        return 0;
+        searchFromStart.BFS2(searchFromFinish); // поиск по ширине с стартового массива
+
+
+        if(searchFromFinish.firstCombNumb >= 0 && result.isEmpty()) {
+            if(searchFromStart.secondCombNumb > 0) // если поиск удался с двух объектов
+                result = searchFromStart.resultList(searchFromStart.firstCombNumb,
+                                                    searchFromStart.secondCombNumb,
+                                                    searchFromFinish
+                                                    ).toString();
+
+            else  // если поиск удался с одного объекта
+                result = searchFromFinish.resultList(searchFromFinish.firstCombNumb).toString();
+
+        }
+        searchFromFinish.BFS2(searchFromStart);  // поиск по ширине с финального массива
+
+        if(!result.isEmpty()) return result;
+        // рекурсия до тех пор пока не будет найдено решение
+        return result(searchFromStart, searchFromFinish);
     }
 
-    public String outResult (int combNumb) {
+    public void BFS2(Search s2) {
+        int[] comb = queueList.poll();
+        int zero = this.zeroCell(comb);
+        for (int i = 0; i < comb.length; i++) {
 
-        if(combNumb == 0) return null;
+            if (this.neighbors(zero, i) &&
+                    comb[i] != HistoryMoves.get(level)) {
+                int[] temp = comb.clone();
+                temp[i] = 0;
+                temp[zero] = comb[i];
+
+                String listStr = Arrays.toString(temp);
+                if (!VisitComb.containsKey(listStr)) {
+                    VisitComb.put(listStr, combNumb);
+                    queueList.add(temp);
+                    HistoryMoves.add(combNumb, comb[i]);
+                    LvlList.add(combNumb, level);
+                    if (Arrays.equals(temp, finish)) firstCombNumb = combNumb;
+                    if (s2.VisitComb.containsKey(listStr)) {  // проверка есть ли такая же комбинация во втором объекте
+                        firstCombNumb = combNumb;
+                        secondCombNumb = s2.VisitComb.get(listStr);
+                    }
+                    combNumb++;
+                }
+            }
+        } level++;
+    }
+
+    // наполнение массива истории передвижения с одного объекта
+    public ArrayList resultList (int combNumb) {
+
+        if(combNumb == 0) return new ArrayList<>();
 
         ArrayList<Integer> tempList = new ArrayList<>();
         tempList.add(HistoryMoves.get(combNumb));
@@ -64,11 +112,18 @@ public class Search {
             combNumb = LvlList.get(combNumb);
             tempList.add(HistoryMoves.get(combNumb));
         }
-        int[] res = new int[tempList.size()];
-        for (int j = 0; j < res.length; j++) {
-            res[j] = tempList.get(res.length - j - 1);
-        }
-        return Arrays.toString(res);
+        //Collections.reverse(tempList);
+        return tempList;
+    }
+
+    // наполнение массива истории передвижения с двух объектов
+    public ArrayList resultList (int secondCombNumb, int firstCombNumb, Search s2) {
+
+        ArrayList<Integer> tempList = resultList(secondCombNumb);
+        Collections.reverse(tempList);
+        tempList.addAll(s2.resultList(firstCombNumb));
+
+        return tempList;
     }
 
     public boolean neighbors(int zero, int a) {
@@ -115,3 +170,4 @@ public class Search {
         return -1;
     }
 }
+
